edfaf9ab14778ccc6c91aa58351ea7ad42b74fde
shondle <106922533+shondle@users.noreply.github.com>
2022-10-15 15:37:59 -0500

    Update PoliceReportHandling.sol

--- PoliceReportHandling.sol
+++ PoliceReportHandling.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Report {
+contract PoliceContract {
     
     struct Report {
         string name;
@@ -27,9 +27,17 @@ contract Report {
     Report[] public reports;    
     mapping (uint => address) public reportToOwner;
 
-    function _createReport(string memory _name, string memory _address, string memory _city, string memory _state, uint _zipcode, uint _phoneNumber, uint _DOB) public {
-        Report newReport = new ReportLabels(_name, _address, _city, _state, _zipcode, _phoneNumber, _DOB);
-        uint id = reports.push(newReport) - 1;
+    function _createReport(string memory _name, string memory _address, string memory _city,
+    string memory _state, uint _zipcode, uint _phoneNumber, 
+    uint[3] memory _DOB, string[] memory _itemsLost, string memory _eventDescription, 
+    string memory _natureComplaint, uint[3] memory _timeIncident, uint[3] memory _dateIncident, uint[3] memory _timeReport,
+    uint[3] memory _dateReport) public {
+
+        Report memory newReport = Report(_name, _address, _city, _state, _zipcode, _phoneNumber, _DOB, _itemsLost, _eventDescription, 
+        _natureComplaint, _timeIncident, _dateIncident, _timeReport,
+        _dateReport);
+        reports.push(newReport);
+        uint id = reports.length-1;
         reportToOwner[id] = msg.sender;
     }
 }
