2bae192b400910d7e3074970bea3ed908be418ae
Sree Duggirala <97408817+sreeduggirala@users.noreply.github.com>
2022-10-15 22:33:46 -0500

    Update PoliceReportHandling.sol

--- PoliceReportHandling.sol
+++ PoliceReportHandling.sol
@@ -13,7 +13,6 @@ contract PoliceContract {
         bytes32 DOB;
         bytes32 phoneNumber;
         bytes32 race;
-        string homeAddress;
         string city;
         string state;
         bytes32 zipCode;
@@ -56,8 +55,8 @@ contract PoliceContract {
     mapping (uint => address) reportToOwner;
 
     //creates new info
-    function _newInfo(address _victim, bytes32 _firstName, bytes32 _lastName, bytes32 _gender, bytes32 _DOB, bytes32 _phoneNumber, bytes32 _race, string memory _homeAddress, string memory _city, string memory _state, bytes32 _zipCode, string memory _country) public {
-        Info memory infoHere = Info(_victim, _firstName, _lastName, _gender, _DOB, _phoneNumber, _race, _homeAddress, _city, _state, _zipCode, _country);
+    function _newInfo(address _victim, bytes32 _firstName, bytes32 _lastName, bytes32 _gender, bytes32 _DOB, bytes32 _phoneNumber, bytes32 _race, string memory _city, string memory _state, bytes32 _zipCode, string memory _country) public {
+        Info memory infoHere = Info(_victim, _firstName, _lastName, _gender, _DOB, _phoneNumber, _race, _city, _state, _zipCode, _country);
         newInfo = infoHere;
     }
 
