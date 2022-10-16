f2d5c80628ae4615fc1a02bbc32ae78f1094122f
shondle <106922533+shondle@users.noreply.github.com>
2022-10-15 21:26:14 -0500

    Update PoliceReportHandling.sol

--- PoliceReportHandling.sol
+++ PoliceReportHandling.sol
@@ -6,13 +6,6 @@ contract PoliceContract {
     
     struct Info {
         address victim;
-        // bytes32 firstName;
-        // bytes32 lastName;
-        // bytes32 gender;
-        // bytes32 DOB;
-        // bytes32 phoneNumber;
-        // bytes32 race;
-        // string homeAddress;
         string city;
         string state;
         bytes32 zipCode;
@@ -20,7 +13,6 @@ contract PoliceContract {
     }
     
     struct Claim {
-    //claims
         bytes32 eventType;
         string eventDescription;
         uint[2] timeIncident;
@@ -32,35 +24,19 @@ contract PoliceContract {
     struct Report {
         Info info;
         Claim claim;
+        PrivInfo priv;
     }
+
+    //initial variables
     Info newInfo;
     Claim newClaim;
     Report[] reports;    
     mapping (uint => address) reportToOwner;
+    address owner; //initialize this
 
     //creates new info
-    function _newInfo(address _victim,
-        // bytes32 _firstName, bytes32 _lastName,bytes32 _gender,
-        // bytes32 _DOB,
-        // bytes32 _phoneNumber,
-        // bytes32 _race,
-        // string memory _homeAddress,
-        string memory _city,
-        string memory _state,
-        bytes32 _zipCode,
-        string memory _country) public {
-        Info memory infoHere = Info(_victim,
-        // _firstName,
-        // _lastName,
-        // _gender,
-        // _DOB,
-        // _phoneNumber,
-        // _race,
-        // _homeAddress,
-        _city,
-        _state,
-        _zipCode,
-        _country);
+    function _newInfo(address _victim, string memory _city, string memory _state, bytes32 _zipCode, string memory _country) public {
+        Info memory infoHere = Info(_victim, _city, _state, _zipCode, _country);
         newInfo = infoHere;
     }
 
@@ -70,12 +46,7 @@ contract PoliceContract {
         uint[3] memory _dateIncident,
         uint[3] memory _timeReport,
         uint[3] memory _dateReport) public {
-        Claim memory claimHere= Claim(_eventType,
-        _eventDescription,
-        _timeIncident,
-        _dateIncident,
-        _timeReport,
-        _dateReport);
+        Claim memory claimHere= Claim(_eventType,_eventDescription,_timeIncident,_dateIncident,_timeReport,_dateReport);
         newClaim = claimHere;
     }
 
